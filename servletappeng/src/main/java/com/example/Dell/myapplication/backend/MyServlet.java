42b05956ac8ce909ca05df15b0206da958c73b92
Dell <rupam.shaw@gmail.com>
2016-08-05 10:53:00 +1000

    initial commit

--- servletappeng/src/main/java/com/example/Dell/myapplication/backend/MyServlet.java
+++ servletappeng/src/main/java/com/example/Dell/myapplication/backend/MyServlet.java
@@ -17,6 +17,7 @@ public class MyServlet extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         resp.setContentType("text/plain");
+
         resp.getWriter().println("Please use the form to POST to this url");
     }
 
